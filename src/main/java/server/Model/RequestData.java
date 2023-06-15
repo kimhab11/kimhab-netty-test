d771595cd2bd8a33694cf3ac5ae701fbd0708639
kimhab <69948342+kimhab11@users.noreply.github.com>
2023-06-15 09:23:07 +0700

    Initial commit

---
+++ src/main/java/server/Model/RequestData.java
@@ -0,0 +1,9 @@
+package server;
+
+import lombok.Data;
+
+@Data
+public class RequestData {
+    private int intValue;
+    private String stringValue;
+}
---
+++ src/main/java/server/Model/ResponseData.java
@@ -0,0 +1,8 @@
+package server;
+
+import lombok.Data;
+
+@Data
+public class ResponseData {
+    private int intValue;
+}
