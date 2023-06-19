ee34072cb4206d09feeea41cdca97c607b91bb72
kimhab <69948342+kimhab11@users.noreply.github.com>
2023-06-19 14:28:32 +0700

    commit

--- src/main/java/LocalIPAddresses.java
+++ src/main/java/LocalIPAddresses.java
@@ -11,47 +11,47 @@ import java.util.List;
 
 public class LocalIPAddresses {
     public static void main(String[] args) throws SocketException {
-//        try {
-//            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
-//            while (networkInterfaces.hasMoreElements()) {
-//                NetworkInterface networkInterface = networkInterfaces.nextElement();
-//                Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
-//                while (inetAddresses.hasMoreElements()) {
-//                    InetAddress inetAddress = inetAddresses.nextElement();
-//                    System.out.println("Local IP Address: " + inetAddress.getHostAddress());
-//                    if (!inetAddress.isLoopbackAddress() && !inetAddress.isLinkLocalAddress() && inetAddress.isSiteLocalAddress()) {
-//                        System.out.println("Local IP Address 1: " + inetAddress.getHostAddress());
-//                    }
-//                }
-//            }
-//        } catch (SocketException e) {
-//            e.printStackTrace();
-//        }
-
-        List<String> hosts =new ArrayList<>();
-        Enumeration<NetworkInterface> allNetInterfaces = NetworkInterface.getNetworkInterfaces();
-        InetAddress ip;
-        while (allNetInterfaces.hasMoreElements()) {
-            NetworkInterface netInterface = allNetInterfaces.nextElement();
-            if (netInterface.isLoopback() || netInterface.isVirtual() || !netInterface.isUp()) {
-                continue;
-            } else if (netInterface.getDisplayName().contains("VMware") || netInterface.getDisplayName().contains("Virtual")) {
-                continue;
-            }
-            Enumeration<InetAddress> addresses = netInterface.getInetAddresses();
-            System.out.println("IP: "+netInterface.getDisplayName());
-            while (addresses.hasMoreElements()) {
-                ip = addresses.nextElement();
-                if (ip instanceof Inet4Address) {
-                    hosts.add(ip.getHostAddress());
+        try {
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = networkInterfaces.nextElement();
+                Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+                while (inetAddresses.hasMoreElements()) {
+                    InetAddress inetAddress = inetAddresses.nextElement();
+                    System.out.println("Local IP Address: " + inetAddress.getHostAddress());
+                    if (!inetAddress.isLoopbackAddress() && !inetAddress.isLinkLocalAddress() && inetAddress.isSiteLocalAddress()) {
+                        System.out.println("Local IP Address 1: " + inetAddress.getHostAddress());
+                    }
                 }
             }
+        } catch (SocketException e) {
+            e.printStackTrace();
         }
-        System.out.println("Server hosts: {}" +hosts);
-        if (hosts.isEmpty()) {
-            throw new RuntimeException("get locale ip error");
-        }
-        System.out.println("K"+hosts);
+
+//        List<String> hosts =new ArrayList<>();
+//        Enumeration<NetworkInterface> allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+//        InetAddress ip;
+//        while (allNetInterfaces.hasMoreElements()) {
+//            NetworkInterface netInterface = allNetInterfaces.nextElement();
+//            if (netInterface.isLoopback() || netInterface.isVirtual() || !netInterface.isUp()) {
+//                continue;
+//            } else if (netInterface.getDisplayName().contains("VMware") || netInterface.getDisplayName().contains("Virtual")) {
+//                continue;
+//            }
+//            Enumeration<InetAddress> addresses = netInterface.getInetAddresses();
+//            System.out.println("IP: "+netInterface.getDisplayName());
+//            while (addresses.hasMoreElements()) {
+//                ip = addresses.nextElement();
+//                if (ip instanceof Inet4Address) {
+//                    hosts.add(ip.getHostAddress());
+//                }
+//            }
+//        }
+//        System.out.println("Server hosts:" +hosts);
+//        if (hosts.isEmpty()) {
+//            throw new RuntimeException("get locale ip error");
+//        }
+//        System.out.println("K"+hosts);
 //        return hosts.get(0);
     }
 }
